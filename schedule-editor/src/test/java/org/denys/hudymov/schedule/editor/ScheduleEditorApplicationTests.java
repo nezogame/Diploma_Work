8030dc7ea81d9fff3bad914be7db228bf18aab31
Denys <denys.hudymov@gmail.com>
2025-04-01 11:38:25 +0300

    auto generated test classes

---
+++ schedule-editor/src/test/java/org/denys/hudymov/schedule/editor/ScheduleEditorApplicationTests.java
@@ -0,0 +1,14 @@
+package org.denys.hudymov.schedule.editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ScheduleEditorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ schedule-editor/src/test/java/org/denys/hudymov/schedule/editor/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package org.denys.hudymov.schedule.editor;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
