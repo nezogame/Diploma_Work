c263638ea6522bc65b04dcec75ed5b0607316ca1
Denys <denys.hudymov@gmail.com>
2025-04-07 11:56:25 +0300

    Add private constructor to prevent instantiation of factory

--- schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/utils/factory/ExcelWorkbookFactory.java
+++ schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/utils/factory/ExcelWorkbookFactory.java
@@ -10,13 +10,17 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 @Slf4j
 public class ExcelWorkbookFactory {
 
+    private ExcelWorkbookFactory() {
+        throw new IllegalStateException("Factory class");
+    }
+
     public static Workbook createWorkbook(String fileName, InputStream is) throws IOException {
         if (fileName.endsWith(".xlsx")) {
             return new XSSFWorkbook(is);
         } else if (fileName.endsWith(".xls")) {
             return new HSSFWorkbook(is);
         } else {
-            log.info("File name: {}", fileName);
+            log.debug("File name: {}", fileName);
             throw new IllegalArgumentException("Invalid file format, only .xls and .xlsx supported");
         }
     }
