c671e50bbe755b2190e7d07455acb9bd12bd6251
Denys <denys.hudymov@gmail.com>
2025-04-21 22:16:17 +0300

    Refactor schedule review and add populate functionality
    Revised `reviewSchedule` to return updated files only when changes are needed and added `populateSchedule` for handling schedules alongside educational programs. Improved logging and streamlined methods for better clarity and functionality.

--- schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/service/impl/ScheduleServiceImpl.java
+++ schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/service/impl/ScheduleServiceImpl.java
@@ -1,29 +1,41 @@
 package org.denys.hudymov.schedule.editor.service.impl;
 
+import dev.langchain4j.service.Result;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Objects;
 import java.util.Optional;
-import org.denys.hudymov.schedule.editor.domain.ScheduleAnalyseResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.denys.hudymov.schedule.editor.domain.ScheduleAnalyseDto;
 import org.denys.hudymov.schedule.editor.llm.assistant.ScheduleAssistant;
+import org.denys.hudymov.schedule.editor.service.ExcelCreatorService;
 import org.denys.hudymov.schedule.editor.service.ExcelIOService;
 import org.denys.hudymov.schedule.editor.service.ScheduleService;
+import org.denys.hudymov.schedule.editor.utils.factory.ExcelWorkbookFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+@Slf4j
 @Service
 public class ScheduleServiceImpl implements ScheduleService {
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ScheduleServiceImpl.class);
     private final ScheduleAssistant scheduleAssistant;
     private final ExcelIOService excelIOService;
+    private final ExcelCreatorService excelCreatorService;
     @Value("${save.folder}")
     private String saveFolder;
 
-    public ScheduleServiceImpl(ScheduleAssistant scheduleAssistant, ExcelIOService excelIOService) {
+    public ScheduleServiceImpl(ScheduleAssistant scheduleAssistant, ExcelIOService excelIOService,
+                               ExcelCreatorService excelCreatorService
+    ) {
         this.scheduleAssistant = scheduleAssistant;
         this.excelIOService = excelIOService;
+        this.excelCreatorService = excelCreatorService;
     }
 
 
@@ -41,20 +53,6 @@ public class ScheduleServiceImpl implements ScheduleService {
         return Optional.ofNullable(scheduleFile);
     }
 
-    @Override
-    public ScheduleAnalyseResponse reviewSchedule(MultipartFile scheduleFile) throws IOException {
-        var scheduleTemplate = excelIOService.readExcelFile(scheduleFile);
-        var scheduleOwerviewResult = scheduleAssistant.generateSchedule(scheduleTemplate);
-        log.info("Tokens was used[{}], finish reason[{}]:, sources[{}]",
-                scheduleOwerviewResult.tokenUsage(),
-                scheduleOwerviewResult.finishReason(),
-                scheduleOwerviewResult.sources()
-
-        );
-        return scheduleOwerviewResult.content();
-    }
-
-
     private String saveScheduleFile(MultipartFile file) throws IOException {
         String dirPath = findCurrentPath();
         createFolder(dirPath);
@@ -62,11 +60,8 @@ public class ScheduleServiceImpl implements ScheduleService {
         return uploadedFile.getAbsolutePath();
     }
 
-    private static File saveFile(MultipartFile file, String dirPath) throws IOException {
-        String fileName = file.getOriginalFilename();
-        File uploadedFile = new File(dirPath, fileName);
-        file.transferTo(uploadedFile);
-        return uploadedFile;
+    private String findCurrentPath() {
+        return Paths.get("").toAbsolutePath().toString();
     }
 
     private void createFolder(String dirPath) throws IOException {
@@ -74,7 +69,69 @@ public class ScheduleServiceImpl implements ScheduleService {
         Files.createDirectory(destFolder.toPath());
     }
 
-    private String findCurrentPath() {
-        return Paths.get("").toAbsolutePath().toString();
+    private static File saveFile(MultipartFile file, String dirPath) throws IOException {
+        String fileName = file.getOriginalFilename();
+        File uploadedFile = new File(dirPath, Objects.requireNonNull(fileName));
+        file.transferTo(uploadedFile);
+        return uploadedFile;
+    }
+
+    @Override
+    public Optional<FileSystemResource> reviewSchedule(MultipartFile scheduleFile) throws IOException {
+        var schedule = excelIOService.readExcelFile(scheduleFile);
+        var scheduleOwerviewResult = scheduleAssistant.generateSchedule(schedule);
+
+        logModelResources(scheduleOwerviewResult);
+
+        var analyseContent = scheduleOwerviewResult.content();
+
+        if (isNotUpdateNeeded(analyseContent)) {
+            return Optional.empty();
+        }
+
+        FileSystemResource updateScheduleFile = updateScheduleFile(scheduleFile, analyseContent);
+        return Optional.ofNullable(updateScheduleFile);
+    }
+
+    private boolean isNotUpdateNeeded(ScheduleAnalyseDto analyseContent) {
+        return analyseContent.changedSubjects().isEmpty();
+    }
+
+    private FileSystemResource updateScheduleFile(
+            MultipartFile scheduleFile,
+            ScheduleAnalyseDto scheduleOverviewResult
+    ) throws IOException {
+
+        var filename = Objects.requireNonNull(scheduleFile.getOriginalFilename());
+        try (InputStream inputStream = scheduleFile.getInputStream();
+             Workbook workbook = ExcelWorkbookFactory.createWorkbook(filename, inputStream)) {
+            excelIOService.applyChangesToExcelFile(workbook, scheduleOverviewResult.changedSubjects());
+            var changedExcelFile = excelCreatorService.createChangedExcelFile(workbook, scheduleFile);
+            log.info("Analyzes comment: {}", scheduleOverviewResult.analyseResult());
+
+            return changedExcelFile;
+        }
+    }
+
+    @Override
+    public FileSystemResource populateSchedule(MultipartFile file, MultipartFile educationalProgramFile) throws IOException {
+        var schedule = excelIOService.readExcelFile(file);
+        var educationalProgram = excelIOService.readExcelFile(educationalProgramFile);
+        var scheduleOwerviewResult = scheduleAssistant.generateSchedule(schedule, educationalProgram);
+
+        logModelResources(scheduleOwerviewResult);
+
+        var analyseContent = scheduleOwerviewResult.content();
+
+        return updateScheduleFile(file, analyseContent);
+    }
+
+    private static void logModelResources(Result<ScheduleAnalyseDto> scheduleOwerviewResult) {
+        log.info("Tokens was used[{}], finish reason[{}]:, sources[{}], tools used {}",
+                scheduleOwerviewResult.tokenUsage(),
+                scheduleOwerviewResult.finishReason(),
+                scheduleOwerviewResult.sources(),
+                scheduleOwerviewResult.toolExecutions()
+        );
     }
 }
