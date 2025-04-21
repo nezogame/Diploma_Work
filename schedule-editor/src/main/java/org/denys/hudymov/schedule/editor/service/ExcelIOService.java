7c8e292cf550cf6851092c78e1902f25d55382ad
Denys <denys.hudymov@gmail.com>
2025-04-21 22:19:48 +0300

    Add method to apply changes to Excel sheets
    Introduced `applyChangesToExcelFile` method in `ExcelIOService` to handle changes to Excel sheets. This includes logic for modifying individual cells, managing merged regions, and handling invalid or overlapping regions. Added detailed logging to track operations and ensure proper modifications within the workbook.

--- schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/service/ExcelIOService.java
+++ schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/service/ExcelIOService.java
@@ -1,14 +1,16 @@
 package org.denys.hudymov.schedule.editor.service;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.denys.hudymov.schedule.editor.domain.ChangedSubjectDto;
 import org.denys.hudymov.schedule.editor.domain.SheetDto;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ExcelIOService {
     Map<String, SheetDto> readExcelFile(MultipartFile file) throws IOException;
 
-    ResponseEntity<FileSystemResource> generateExcelFile();
+    void applyChangesToExcelFile(Workbook file, Map<String, List<ChangedSubjectDto>> changes);
+
 }
--- schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/service/impl/ExcelIOServiceImpl.java
+++ schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/service/impl/ExcelIOServiceImpl.java
@@ -6,25 +6,33 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.denys.hudymov.schedule.editor.domain.CellDto;
+import org.denys.hudymov.schedule.editor.domain.ChangedSubjectDto;
+import org.denys.hudymov.schedule.editor.domain.CoordinatesDto;
+import org.denys.hudymov.schedule.editor.domain.MergedRegion;
 import org.denys.hudymov.schedule.editor.domain.RowDto;
 import org.denys.hudymov.schedule.editor.domain.SheetDto;
 import org.denys.hudymov.schedule.editor.service.ExcelIOService;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import static org.denys.hudymov.schedule.editor.llm.tool.MergedRegionTool.isRegionInsertable;
+import static org.denys.hudymov.schedule.editor.llm.tool.MergedRegionTool.isSingleCellMergedRegion;
 import static org.denys.hudymov.schedule.editor.utils.excel.MergedRegionUtil.setCellInMergedRegion;
 import static org.denys.hudymov.schedule.editor.utils.factory.ExcelWorkbookFactory.createWorkbook;
 
+@Slf4j
 @Service
 public class ExcelIOServiceImpl implements ExcelIOService {
     @Override
@@ -73,7 +81,105 @@ public class ExcelIOServiceImpl implements ExcelIOService {
     }
 
     @Override
-    public ResponseEntity<FileSystemResource> generateExcelFile() {
-        return null;
+    public void applyChangesToExcelFile(Workbook workbook, Map<String, List<ChangedSubjectDto>> changes) {
+        changes.forEach(
+                (sheetName, change) -> applyChangeToEverySheet(workbook, sheetName, change)
+        );
+    }
+
+    private void applyChangeToEverySheet(Workbook workbook, String sheetName, List<ChangedSubjectDto> changes) {
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        if (sheet == null) {
+            log.warn("Sheet [{}] not found in the workbook. Skipping changes for this sheet.", sheetName);
+            return;
+        }
+
+        List<Cell> cells = applyChangesToCells(changes, sheet);
+        log.info("Applied change to sheet [{}], cells {}",
+                sheetName, cells.stream().map(Cell::getStringCellValue).toList());
+    }
+
+    private List<Cell> applyChangesToCells(List<ChangedSubjectDto> changes, Sheet sheet) {
+        return changes.stream()
+                .map(changeCell(sheet))
+                .toList();
+    }
+
+    private Function<ChangedSubjectDto, Cell> changeCell(Sheet sheet) {
+        return change -> {
+            MergedRegion mergedRegion = change.newChange().mergedRegion();
+            if (isSingleCellMergedRegion(mergedRegion)) {
+                log.warn("Incorrect region for the cell [{}]", change.newChange());
+                change = ChangedSubjectDto.builder()
+                        .newChange(
+                                CellDto.builder()
+                                        .data(change.newChange().data())
+                                        .coordinate(change.newChange().coordinate())
+                                        .mergedRegion(MergedRegion.EMPTY_REGION)
+                                        .isMergedStart(false)
+                                        .build()
+                        )
+                        .build();
+            } else if (!isRegionInsertable(sheet, mergedRegion)) {
+                log.warn("Region {} is overlapping with another one. Skipping changes for this sheet.", mergedRegion);
+                return getCell(sheet, change.newChange().coordinate());
+            }
+
+            if (mergedRegion.isEmptyRegion()) {
+                return changeSingleCell(sheet, change);
+            }
+            return changeRegionCell(sheet, change);
+        };
+    }
+
+    private Cell changeSingleCell(Sheet sheet, ChangedSubjectDto change) {
+        CoordinatesDto coordinates = change.newChange().coordinate();
+        Cell cell = getCell(sheet, coordinates);
+        cell.setCellValue(change.newChange().data());
+        return cell;
+    }
+
+    private Cell getCell(Sheet sheet, CoordinatesDto coordinates) {
+        Row row = sheet.getRow(coordinates.row());
+        Cell cell = row.getCell(coordinates.col(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+        return cell;
+    }
+
+    private Cell changeRegionCell(Sheet sheet, ChangedSubjectDto change) {
+        MergedRegion mergedRegion = change.newChange().mergedRegion();
+
+        CellRangeAddress targetRegion = new CellRangeAddress(
+                mergedRegion.firstRow(), mergedRegion.lastRow(),
+                mergedRegion.firstColumn(), mergedRegion.lastColumn()
+        );
+
+        boolean regionExists = isRegionExists(sheet, targetRegion);
+
+        if (!regionExists) {
+            log.debug("Created region {}", targetRegion);
+            sheet.addMergedRegion(targetRegion);
+        }
+        var changedCell = changeSingleCell(sheet, change);
+        unmergeCellIfEmpty(sheet, change, targetRegion);
+
+        return changedCell;
+    }
+
+    private boolean isRegionExists(Sheet sheet, CellRangeAddress targetRegion) {
+        return sheet.getMergedRegions()
+                .stream()
+                .anyMatch(existingRegion -> existingRegion.equals(targetRegion));
+    }
+
+    private void unmergeCellIfEmpty(Sheet sheet, ChangedSubjectDto change, CellRangeAddress targetRegion) {
+        MergedRegion mergedRegion = change.newChange().mergedRegion();
+
+        Cell firstCell = sheet.getRow(mergedRegion.firstRow())
+                .getCell(mergedRegion.firstColumn(), Row.MissingCellPolicy.CREATE_NULL_AS_BLANK);
+
+        if (firstCell.getStringCellValue().isBlank()) {
+            sheet.removeMergedRegion(sheet.getMergedRegions().indexOf(targetRegion));
+        }
     }
 }
