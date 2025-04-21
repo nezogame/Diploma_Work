3ff3df3a8ec399f2c82afaf3c7fd4f8638641654
Denys <denys.hudymov@gmail.com>
2025-04-21 22:21:32 +0300

    Refactor and enhance LLM configuration and region validation
    Add new beans for embedding model and message memory while refactoring `LlmConfiguration`. Improve `MergedRegionTool` logic to better validate merged regions and enhance logging. Update dependencies and configuration properties for better timeout handling and versioning.

--- schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/config/LlmConfiguration.java
+++ schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/config/LlmConfiguration.java
@@ -1,9 +1,12 @@
 package org.denys.hudymov.schedule.editor.config;
 
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.listener.ChatModelErrorContext;
 import dev.langchain4j.model.chat.listener.ChatModelListener;
 import dev.langchain4j.model.chat.listener.ChatModelRequestContext;
 import dev.langchain4j.model.chat.listener.ChatModelResponseContext;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.bgesmallenv15q.BgeSmallEnV15QuantizedEmbeddingModel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,8 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 public class LlmConfiguration {
+    public static final int MAX_MESSAGES = 10;
+
     @Bean
     ChatModelListener chatModelListener() {
         return new ChatModelListener() {
@@ -29,5 +34,23 @@ public class LlmConfiguration {
                 log.info("onError(): {}", errorContext.error().getMessage());
             }
         };
+    }
+
+/*    private void addToolChoiceToRequest(ChatModelRequestContext requestContext) {
+        var parameters = ChatRequestParameters.builder()
+                .toolChoice(ToolChoice.REQUIRED)
+                .build();
+        ChatRequestParameters chatRequestParameters = requestContext.chatRequest().parameters().overrideWith(parameters);
+
+    }*/
+
+    @Bean
+    EmbeddingModel embeddingModel() {
+        return new BgeSmallEnV15QuantizedEmbeddingModel();
+    }
+
+    @Bean
+    MessageWindowChatMemory messageWindowChatMemory() {
+        return MessageWindowChatMemory.withMaxMessages(MAX_MESSAGES);
     }
 }
--- schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/llm/tool/MergedRegionTool.java
+++ schedule-editor/src/main/java/org/denys/hudymov/schedule/editor/llm/tool/MergedRegionTool.java
@@ -10,23 +10,31 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class MergedRegionTool {
+    MergedRegionTool() {
+        throw new IllegalStateException("Utility class");
+    }
 
     @Tool(name = "is_single_cell_merged_region", value = "Validates whether the provided merged region " +
             "represents a single cell. In case if single region mergedRegion must be replaced with EMPTY_REGION")
-    public boolean isSingleCellMergedRegion(MergedRegion mergedRegion) {
-        return mergedRegion.firstRow() == mergedRegion.lastRow() &&
-                mergedRegion.firstColumn() == mergedRegion.lastColumn();
+    public static boolean isSingleCellMergedRegion(MergedRegion mergedRegion) {
+        if (mergedRegion == null) {
+            return true;
+        }
+
+        var numberOfCell = (mergedRegion.lastRow() - mergedRegion.firstRow() + 1) *
+                (mergedRegion.lastColumn() - mergedRegion.firstColumn() + 1);
+        return numberOfCell < 2;
     }
 
     @Tool(name = "is_region_insertable", value = "Validates whether it is possible to insert a " +
             "region without overlapping with existing merged regions.")
-    public boolean isRegionInsertable(Sheet sheet, MergedRegion mergedRegion) {
+    public static boolean isRegionInsertable(Sheet sheet, MergedRegion mergedRegion) {
         CellRangeAddress newRegion = new CellRangeAddress(
                 mergedRegion.firstRow(), mergedRegion.lastRow(),
                 mergedRegion.firstColumn(), mergedRegion.lastColumn()
         );
 
-        log.info("Validating region [{}]", newRegion.formatAsString());
+        log.info("At sheet [{}] Validating region [{}]", sheet.getSheetName(), newRegion.formatAsString());
         return sheet.getMergedRegions()
                 .stream()
                 .noneMatch(existingRegion -> existingRegion.intersects(newRegion));
